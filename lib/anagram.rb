ae49b36f026dd1d4bf73b31d5ebeb8f0f5920476
Ibaihima <ibaihima@hotmail.com>
2022-03-16 16:05:47 -0400

    commit tree 085067ae5dc00a3f440b077ba0819f5747aa94a9

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    
+    attr_accessor :word
+    def initialize word
+        @word = word
+        # binding.pry
+    end 
+    def match new_word_array
+        array = []
+        new_word_array.each do |string| 
+             single_letters_word = @word.chars
+             single_letter_string = string.chars 
+            if (single_letter_string.sort == single_letters_word.sort) 
+                array << string
+            end
+        end
+        return array
+    end
+end
